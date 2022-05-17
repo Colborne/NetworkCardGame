1f4ddc27c2001b7783aaef9bb048a51e26379c11
Logan Colborne <Logan.Colborne@gmail.com>
2022-05-17 09:43:41 -0400

    idk some fixes

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
     public int slotNumber;
     public bool rot;
+
+    private void Update() {
+        if(rot)
+            GetComponent<Image>().color = new Color32(60,85,60,255);
+        else
+            GetComponent<Image>().color = new Color32(255,255,255,255);
+    }
 }
